5db37a6c03a0bd8cbf1b36ba62b323565c552329
taihan khane <taihankhan86@gmail.com>
2024-05-31 20:03:01 +0530

    added deals components

--- ecommerce-web/src/app/page.js
+++ ecommerce-web/src/app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Header from "./components/Header";
-import Banner from "./components/Banner"
-
+import Banner from "./components/Banner";
+import Deals from "./components/Deals"
+import  ProductFeed from "./components/ProductFeed"
 export default function Home() {
   return (
     <main className="">
       <Header/>
       <Banner/>
+      <Deals/>
+      <ProductFeed/>
     </main>
   );
 }
