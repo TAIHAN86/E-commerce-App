54fde190001d840c086ed9199b88f3e9050ef202
taihan khane <taihankhan86@gmail.com>
2024-05-31 12:26:47 +0530

    Navbar done

--- ecommerce-web/src/app/page.js
+++ ecommerce-web/src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>let build the ecommerce web app</h2>
+    <main className="">
+      <Header/>
     </main>
   );
 }
