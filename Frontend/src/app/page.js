26cab3ae25c6391df30ac04083c0b2b3572b6dc1
taihan khane <taihankhan86@gmail.com>
2024-06-05 12:05:37 +0530

    directory name change

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Deals from "./components/Deals"
 import  ProductFeed from "./components/ProductFeed"
-import Product from "./components/Product";
+import Product from "./components/product";
 
 export default function Home() {
   return (
