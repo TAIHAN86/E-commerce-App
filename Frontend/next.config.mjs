ffd42ae932d264d2a030f22edbcb5caeb9648063
taihan khane <taihankhan86@gmail.com>
2024-06-10 13:39:48 +0530

    cart screen is created using funtionality of decrementQuantity     incrementQuantity using redux cart reducer for product.js or fakestore api product

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [
+        "images-eu.ssl-images-amazon.com",
+        "fakestoreapi.com",
+        "m.media-amazon.com",
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
